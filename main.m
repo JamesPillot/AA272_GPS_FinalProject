b5d9ee9f51cf7c19d44d3346b71d18522373c66b
James P <38028854+JamesPillot@users.noreply.github.com>
2022-02-23 20:48:25 -0800

    Basic Dynamics Implemented
    Dynamics implemented and tested with basic cases. State tracking is probably next

--- main.m
+++ main.m
@@ -1,12 +1,24 @@
 %% System parameters - feel free to change these to be more realistic
-m = 1000; % Mass of air taxi - in kg
-Jcm_zz = 20; % Inertia of air taxi about z-axis going through center of mass - in kg m^2 
-L = 10; % Distance from left thruster to right thruster - in meters
-delta_t = 0.001; % Delta in time - in seconds
+close all; clear all;
+params.m = 1000; % Mass of air taxi - in kg
+params.Jcm_zz = 200; % Inertia of air taxi about z-axis going through center of mass - in kg m^2 
+params.L = 10; % Distance from left thruster to right thruster - in meters
+params.delta_t = 0.001; % Delta in time - in seconds
 %% Dynamics and Simulation, No Measurement Errors
-x = [0;0;0;0;0;0]; % initially at rest at origin
-x_dot = [0;0;0;0;0;0]; % initially at rest
-% x_next = 
+x0 = [0;0;0;0;0;0]; % initially at rest at origin, state x is [x; y; theta; xdot; ydot; thetadot]
+time_to_run = 15; % in seconds
+input_length = time_to_run / params.delta_t;
+% Force input
+left_thrust = 20*(ones(input_length,1));
+right_thrust = -20*(ones(input_length,1));
+u = [left_thrust, right_thrust];
+% Simulate Dynamics and Log
+[meas_log, state_log] = simSystem(x0, u, time_to_run, params);
+
+%% Plot
+t = linspace(0,time_to_run,length(state_log));
+figure;
+plot(t, sin(state_log(:,3)));
 %% EKF Modifications to Track with Imperfect Measurements
 
 
--- simSystem.m
+++ simSystem.m
@@ -1,11 +1,41 @@
-function [meas, state] = simSystem(x0,xdot0, u, time_to_run, parmams)
+function [meas_log, state_log] = simSystem(x0, u, time_to_run, params)
 % Simulates system
 % Takes in initial conditions (x0 and xdot0), system inputs (u), the
-% time to run (time_to_run), and the system parameters (params), and 
-% returns a log of the state and the measured variables of the system
+% time to run (time_to_run), and the system parameters (params, a struct), and 
+% returns a log of the state (state_log) and the measured variables (meas_log) of the system
 
-t = 0;
+% Set up Logging
+entry_index = 1; 
+state_size = 6; % number of states
+meas_size = 2; % number of measurements - 1 for x and 1 for y
+num_entries = time_to_run/params.delta_t; % number of entries to log
+state_log = zeros(num_entries, state_size); % log states over time
+meas_log = zeros(num_entries, meas_size); % log measurements over time
+
+% Log initial state
+state_log(entry_index,:) = x0;
+meas_log(entry_index,:) = [x0(1); x0(2)];
+entry_index = entry_index + 1;
+
+% Simulate Dynamics and Log
+t = params.delta_t; % Since we already logged t = 0 entry, start at t = 0 + delta_t
+x_last = x0;
 while(t < time_to_run)
+    % Propagate state forward
+    left_thrust = u(entry_index - 1, 1); % (entry_index - 1) so that at first time step we reference first input
+    right_thrust = u(entry_index - 1, 2); 
+    x_dot = [x_last(4); x_last(5); x_last(6); ...
+        ((left_thrust+right_thrust)/params.m)*cos(x_last(3)); ((left_thrust+right_thrust)/params.m)*sin(x_last(3)); ((params.L)/2)*(right_thrust - left_thrust)*(1/params.Jcm_zz)];
+    
+    x_next = x_last + params.delta_t*x_dot;
+    % Take measurement
+    curr_meas = [x_next(1); x_next(2)];
+    % Log
+    state_log(entry_index,:) = x_next;
+    meas_log(entry_index,:) = curr_meas;
 
+    x_last = x_next;
+    entry_index = entry_index + 1;
+    t = t + params.delta_t;
 end
 end
