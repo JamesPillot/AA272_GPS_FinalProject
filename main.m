d42895c534e8db949a481f9efb940a7bd5dda8e3
James P <38028854+JamesPillot@users.noreply.github.com>
2022-03-03 01:03:31 -0800

    Swapped Model, Made Plots
    Now need to update measurement model and add in satellite locations

--- main.m
+++ main.m
@@ -1,25 +1,72 @@
 %% System parameters - feel free to change these to be more realistic
 close all; clear all;
 params.m = 1000; % Mass of air taxi - in kg
-params.Jcm_zz = 200; % Inertia of air taxi about z-axis going through center of mass - in kg m^2 
-params.L = 10; % Distance from left thruster to right thruster - in meters
 params.delta_t = 0.001; % Delta in time - in seconds
-%% Dynamics and Simulation, No Measurement Errors
-x0 = [0;0;0;0;0;0]; % initially at rest at origin, state x is [x; y; theta; xdot; ydot; thetadot]
-time_to_run = 15; % in seconds
-input_length = time_to_run / params.delta_t;
+rng(1); % For repeatability
+%% 1. Dynamics and Simulation, True Path
+x0 = [0;0;0;0]; % initially at rest at origin, state x is [x; y; xdot; ydot]
+time_to_run = 10; % in seconds
+t = (0:params.delta_t:time_to_run)';
 % Force input
-left_thrust = 20*(ones(input_length,1));
-right_thrust = -20*(ones(input_length,1));
-u = [left_thrust, right_thrust];
+fx = 500*cos(.4*t);
+fy = 1000*sin(.05*t);
+u = [fx, fy];
 % Simulate Dynamics and Log
-[meas_log, state_log] = simSystem(x0, u, time_to_run, params);
+Q = zeros(2,2);
+R = zeros(2,2);
+[true_path_meas_log, true_path_state_log] = simSystem(x0, u, time_to_run, params, Q,R);
 
-%% Plot
-t = linspace(0,time_to_run,length(state_log));
+%% 1. Plot True Path with Trees
+close all;
+tree1_location = [4.5, 1];
+tree2_location = [5.9, 6];
 figure;
-plot(t, sin(state_log(:,3)));
-%% EKF Modifications to Track with Imperfect Measurements
+hold on;
+x_positions = true_path_meas_log(:,1);
+y_positions = true_path_meas_log(:,2);
+plot(x_positions, y_positions, '--');
+plot(tree1_location(1), tree1_location(2),  'g^', 'MarkerFaceColor', 'green', 'MarkerSize', 14);
+plot(tree2_location(1), tree2_location(2),  'g^', 'MarkerFaceColor', 'green', 'MarkerSize', 14);
+xlabel('X Position (meters)', 'Interpreter', 'latex', 'Fontsize', 14);
+ylabel('Y Position (meters)', 'Interpreter', 'latex', 'Fontsize', 14);
+title('True Vehicle Trajectory', 'Interpreter', 'latex', 'Fontsize', 16);
+legend('Vehicle Path','Tree 1', 'Tree 2','Interpreter', 'latex','Fontsize', 12, 'Location', 'northwest');
+grid on;
+hold off;
+%% 2. Dynamics and Simulation, Fixed Variance, Gaussian White Noise
+x0 = [0;0;0;0]; % initially at rest at origin, state x is [x; y; xdot; ydot]
+time_to_run = 10; % in seconds
+t = (0:params.delta_t:time_to_run)';
+% Force input
+fx = 500*cos(.4*t);
+fy = 1000*sin(.05*t);
+u = [fx, fy];
+% Simulate Dynamics and Log
+Q = 1*10^-5*eye(2,2);
+R = 5*10^-5*eye(2,2);
+[fixed_var_path_meas_log, fixed_var_path_state_log] = simSystem(x0, u, time_to_run, params, Q,R);
+%% 2. Plot True Path with Trees vs. Path with Fixed Variance, Gaussian White Noise
+close all;
+tree1_location = [4.5, 1];
+tree2_location = [5.9, 6];
+figure;
+hold on;
+x_positions = true_path_meas_log(:,1);
+y_positions = true_path_meas_log(:,2);
+x_positions_fixed_var = fixed_var_path_meas_log(:,1);
+y_positions_fixed_var = fixed_var_path_meas_log(:,2);
+
+plot(x_positions, y_positions, 'b--');
+plot(x_positions_fixed_var, y_positions_fixed_var, 'r--')
+plot(tree1_location(1), tree1_location(2),  'g^', 'MarkerFaceColor', 'green', 'MarkerSize', 14);
+plot(tree2_location(1), tree2_location(2),  'g^', 'MarkerFaceColor', 'green', 'MarkerSize', 14);
+xlabel('X Position (meters)', 'Interpreter', 'latex', 'Fontsize', 14);
+ylabel('Y Position (meters)', 'Interpreter', 'latex', 'Fontsize', 14);
+title('True Vehicle Trajectory and Vehicle Trajectory with Fixed Variance Noise', 'Interpreter', 'latex', 'Fontsize', 16);
+legend('Vehicle Path','Tree 1', 'Tree 2','Interpreter', 'latex','Fontsize', 12, 'Location', 'northwest');
+grid on;
+hold off;
+%% 3. Dynamics and Simulation, Time-Varying Variance, Gaussian White Noise
 
 
 
--- simSystem.m
+++ simSystem.m
@@ -1,13 +1,14 @@
-function [meas_log, state_log] = simSystem(x0, u, time_to_run, params)
+function [meas_log, state_log] = simSystem(x0, u, time_to_run, params, Q, R)
 % Simulates system
 % Takes in initial conditions (x0 and xdot0), system inputs (u), the
 % time to run (time_to_run), and the system parameters (params, a struct), and 
 % returns a log of the state (state_log) and the measured variables (meas_log) of the system
+% Q is process noise covariance matrix, R is sensor noise covariance matrix
 
 % Set up Logging
 entry_index = 1; 
-state_size = 6; % number of states
-meas_size = 2; % number of measurements - 1 for x and 1 for y
+state_size = 4; % number of states
+meas_size = 2; % number of measurements - 1 for position in x and 1 for position in y
 num_entries = time_to_run/params.delta_t; % number of entries to log
 state_log = zeros(num_entries, state_size); % log states over time
 meas_log = zeros(num_entries, meas_size); % log measurements over time
@@ -22,14 +23,16 @@ t = params.delta_t; % Since we already logged t = 0 entry, start at t = 0 + delt
 x_last = x0;
 while(t < time_to_run)
     % Propagate state forward
-    left_thrust = u(entry_index - 1, 1); % (entry_index - 1) so that at first time step we reference first input
-    right_thrust = u(entry_index - 1, 2); 
-    x_dot = [x_last(4); x_last(5); x_last(6); ...
-        ((left_thrust+right_thrust)/params.m)*cos(x_last(3)); ((left_thrust+right_thrust)/params.m)*sin(x_last(3)); ((params.L)/2)*(right_thrust - left_thrust)*(1/params.Jcm_zz)];
-    
-    x_next = x_last + params.delta_t*x_dot;
+    fx = u(entry_index - 1, 1); % (entry_index - 1) so at first time step we reference first input
+    fy = u(entry_index - 1, 2); 
+    x_dot = [x_last(3); x_last(4); fx/params.m; fy/params.m];
+    W = sqrt(Q)*randn(2,1);
+    process_noise = [0; 0; W(1); W(2)];
+    x_next = x_last + params.delta_t*x_dot + process_noise;
     % Take measurement
-    curr_meas = [x_next(1); x_next(2)];
+    V = sqrt(R)*randn(2,1);
+    sensor_noise = [V(1); V(2)];
+    curr_meas = [x_next(1); x_next(2)] + sensor_noise;
     % Log
     state_log(entry_index,:) = x_next;
     meas_log(entry_index,:) = curr_meas;
